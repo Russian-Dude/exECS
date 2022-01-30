ffbb6d1c8482ab7720e63a3fac90c45c3051fcfc
Russian-Dude <ghritnik@gmail.com>
2022-01-30 03:19:21 +0300

    Fixed adding vararg components to entity

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.russian-dude"
-version = "1.0.4"
+version = "1.0.5"
 
 repositories {
     mavenCentral()
@@ -30,7 +30,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.russian-dude"
             artifactId = "execs"
-            version = "1.0.4"
+            version = "1.0.5"
             from(components["java"])
         }
     }
