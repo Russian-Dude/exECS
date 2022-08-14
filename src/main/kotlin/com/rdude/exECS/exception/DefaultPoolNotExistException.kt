4cc437f48cb21eefde3da12faf616c843250d8f6
Russian-Dude <ghritnik@gmail.com>
2022-08-14 15:31:11 +0300

    DefaultPoolNotExistException with better message

--- src/main/kotlin/com/rdude/exECS/exception/DefaultPoolNotExistException.kt
+++ src/main/kotlin/com/rdude/exECS/exception/DefaultPoolNotExistException.kt
@@ -4,4 +4,7 @@ import com.rdude.exECS.pool.Poolable
 import kotlin.reflect.KClass
 
 class DefaultPoolNotExistException internal constructor(type: KClass<out Poolable>) : ExEcsException(
-    "Can not obtain Poolable instance of $type from the default Pool. Default Pool for this type does not exist")
+    "Can not obtain Poolable instance of $type from the default Pool. Default Pool for this type does not exist. " +
+            "Creating a default Pool requires a constructor in which all arguments have a default value.") {
+
+}
