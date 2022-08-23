c605bd2acdcce0f42b0d6447a44725c7bc94fefa
Russian-Dude <ghritnik@gmail.com>
2022-08-23 15:25:31 +0300

    Better exception message

--- src/main/kotlin/com/rdude/exECS/exception/PoolNotSetException.kt
+++ src/main/kotlin/com/rdude/exECS/exception/PoolNotSetException.kt
@@ -1,11 +1,14 @@
 package com.rdude.exECS.exception
 
+import com.rdude.exECS.config.ExEcsGlobalConfiguration
+import com.rdude.exECS.config.OnPoolIsNotSet
 import com.rdude.exECS.pool.Poolable
-import com.rdude.exECS.pool.Pool
 import kotlin.reflect.KClass
 
-/** Throws if [Poolable.returnToPool] method called but [Poolable.pool] property is null.
- * [Poolable] instance was probably created manually instead of being created by the [Pool.obtain] method.*/
-class PoolNotSetException internal constructor(type: KClass<out Poolable>): ExEcsException(
+/** Throws if [Poolable.returnToPool] method called but [Poolable.pool] property is null and
+ * [ExEcsGlobalConfiguration.onPoolIsNotSet] is set to [OnPoolIsNotSet.THROW].*/
+class PoolNotSetException internal constructor(type: KClass<out Poolable>) : ExEcsException(
     "Can not return Poolable instance of the $type to the Pool. Pool property of the instance is null. " +
-            "Poolable instance was probably created manually instead of being created by the Pool.obtain method.")
+            "Poolable instance was probably created manually instead of being created by the Pool.obtain method. " +
+            "This exception was thrown because ExEcsGlobalConfiguration.onPoolIsNotSet property is set to OnPoolIsNotSet.THROW."
+)
