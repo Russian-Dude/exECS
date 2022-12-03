cc697223612a35990ffebbec9288d611bd345139
Russian-Dude <ghritnik@gmail.com>
2022-12-03 17:06:46 +0300

    Removed entityAsSingleton property from EntityAddedEvent and EntityRemovedEvent

--- src/main/kotlin/com/rdude/exECS/event/EntityRemovedEvent.kt
+++ src/main/kotlin/com/rdude/exECS/event/EntityRemovedEvent.kt
@@ -1,7 +1,6 @@
 package com.rdude.exECS.event
 
 import com.rdude.exECS.entity.Entity
-import com.rdude.exECS.entity.SingletonEntity
 import com.rdude.exECS.pool.Pool
 import com.rdude.exECS.system.EventSystem
 import com.rdude.exECS.world.World
@@ -11,10 +10,6 @@ class EntityRemovedEvent internal constructor() : InternalPoolableEvent() {
 
     /** The [Entity] that was removed from the [World].*/
     var entity: Entity = Entity.NO_ENTITY
-        internal set
-
-    /** The [entity] as a [SingletonEntity] or null if it is not [SingletonEntity].*/
-    var entityAsSingleton: SingletonEntity? = null
         internal set
 
     // hardcoded for performance
