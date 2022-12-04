f211be401e42296d60241bb149787635e9395757
Russian-Dude <ghritnik@gmail.com>
2022-12-04 23:27:35 +0300

    Changed version to 1.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 group = "com.russian-dude"
-version = "1.5.0"
+version = "1.5.1"
 
 repositories {
     mavenCentral()
@@ -37,7 +37,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.russian-dude"
             artifactId = "execs"
-            version = "1.5.0"
+            version = "1.5.1"
             from(components["java"])
         }
     }
